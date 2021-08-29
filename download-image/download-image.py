56a30a8464c112ebf2b3f4418b8e4897bd292b4a
6ust <gust.guedes95@gmail.com>
2021-08-29 11:24:16 -0300

    incluindo script download-image

---
+++ download-image/download-image.py
@@ -0,0 +1,23 @@
+# CRIAR UM SCRIPT QUE UTILIZE UMA PLANILHA COM LINKS PARA BAIXAR PHOTOS 
+# E SALVAR COM O NOME E TIPO INFORMADOS EM UMA COLUNA ESPECIFICA
+
+#É NECESSARIO TER A BIBLIOTECA 'REQUESTS' INSTALADA
+# pip install requests
+import requests
+
+imageURL = "https://external-preview.redd.it/K0CT6pvyCGPOFEyjfli8Y7hJUg5X_0IO86yasq0LCRY.jpg?auto=webp&s=f5b24a2a37993ea22ba3c4e68a870bbb0837265a"
+
+with open('binary.png', 'wb') as imagem:
+  resposta = requests.get(imageURL, stream=True)
+
+  if not resposta.ok:
+    print("Ocorreu um erro, status:" , resposta.status_code)
+  else:
+    for dado in resposta.iter_content(1024):
+      if not dado:
+          break
+
+      imagem.write(dado)
+
+    print("Imagem salva! =)")
+
